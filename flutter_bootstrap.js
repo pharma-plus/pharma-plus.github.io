8ac85c123c9962f9ce79cfe1afcc6297766f0f78
Merouan <merouan@pharmaplus.com>
2026-08-27 17:50:00 +0100

    Dashboard ultra-compact: cards petites, blancs minimises, tient dans viewport sans scroll

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4290604964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3627943091" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
