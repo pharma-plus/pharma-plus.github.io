8fb77c1116dcfa7ff8c2241cc87f97d33fb419cb
Merouan <merouan@pharmaplus.com>
2026-08-30 15:09:42 +0100

    Sidebar: vrai logo PHARMA+ a la place de l'icone croix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2202253264" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4167609948" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
