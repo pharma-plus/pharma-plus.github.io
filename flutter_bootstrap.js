f0b10ed78a08e79f7cf6f257d9750af305ba637b
PHARMA+ Dev <dev@pharma-maroc.local>
2026-09-24 09:59:03 +0000

    BUILD 280 deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,11 +33,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c096","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.202609240717.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c096","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.202609240958.dart.js"},{}]};
 
 _flutter.loader.load({
   config: { renderer: "html" },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "409328546" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3167688924" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
